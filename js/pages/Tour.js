a131f5eadeb2c815889b2f742465670b1488fd54
AllanFM <AllanFM24@gmail.com>
2016-08-27 21:25:13 +0200

    Git ignore added and JSON

--- js/pages/Tour.js
+++ js/pages/Tour.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default class Tour extends React.Component{
     render(){
-        return <h2>Tour</h2>
+        return (
+            <main>
+              <a href="http://www.bandsintown.com/Bound%20By%20Law" data-share-links="false" class="bit-widget-initializer" data-text-color="#fff" data-link-color="lightgray" data-artist="Bound By Law">
+                 Bound By Law Tour Dates
+              </a>
+            </main>
+        )
     }
 }
--- js/pages/Videos.js
+++ js/pages/Videos.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import YoutubeList from '../../services/videos.json';
 
 export default class Videos extends React.Component{
+    renderVideos(){
+      //console.log(YoutubeList);
+      var text = "", url = "";
+      var x;
+      for (x in YoutubeList) {
+
+          url = YoutubeList[x].url;
+          text += "<iframe width='560' height='315' src='" + url + "' frameborder='0' allowfullscreen></iframe>";
+
+      }
+      return text;
+    }
+
     render(){
-        return <h2>Videos</h2>
+        return (
+          <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/JkRfp63MOjk" frameborder="0" allowfullscreen></iframe>
+
+              {this.renderVideos()}
+          </div>
+        )
     }
 }
